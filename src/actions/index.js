ec4a29483c886ce8ace7248338a6124e7c58c795
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 13:44:27 -0700

    added login user fail

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import firebase from 'firebase';
 import { 
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
  } from './types';
 
 export const emailChanged = (text) => {
@@ -29,6 +30,10 @@ export const loginUser = ({ email, password }) => {
           .then(user => loginUserSuccess(dispatch, user));
       });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
 };
 
 const loginUserSuccess = (dispatch, user) => {
