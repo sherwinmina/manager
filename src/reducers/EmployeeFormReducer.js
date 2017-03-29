841af85411d105885ee534be62d9b68e444718a4
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 16:12:04 -0700

    Added employee FormReducer

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,13 @@
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types';
+
+
+const INITIAL_STATE = {};
+
+  export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      default: 
+        return state;
+    }
+  };
