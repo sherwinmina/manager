1a7c8e1eb8c36f266786a91ff1219ea8a21f3619
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 00:18:39 -0700

    firebase set up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+
+import reducers from './reducers';
+
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyBDq21TSGVrnj1wFgnvclEbf65ag2lblaA",
+      authDomain: "manager-e9bc1.firebaseapp.com",
+      databaseURL: "https://manager-e9bc1.firebaseio.com",
+      storageBucket: "manager-e9bc1.appspot.com",
+      messagingSenderId: "303896157595"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore()}>
