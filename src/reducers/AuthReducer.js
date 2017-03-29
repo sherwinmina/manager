fb2b2be321cdaccf6bb91b73af90f96354ccfd90
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 01:46:47 -0700

    added type.js to actions folder

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,5 +1,6 @@
-const INITIAL_STATE = { email: '' };
+import { EMAIL_CHANGED } from '../actions/types';
 
+const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
