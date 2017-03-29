2b19de15a23e5ebed84caf1e232530991c0c3519
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 16:26:20 -0700

    Added Auth Actions

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,10 +3,16 @@ import {
 } from '../actions/types';
 
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
   export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+      case EMPLOYEE_UPDATE:
+        return { ...state, [action.payload.prop]: action.payload.value };
       default: 
         return state;
     }
