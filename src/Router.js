5af17e11596c56da553ba45100ff46b1380ebdec
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-29 15:47:26 -0700

    added employee create component

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -11,7 +12,15 @@ const RouterComponent = () => {
       </Scene>   
 
       <Scene key="main">
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        <Scene 
+          onRight={() => Actions.employeeCreate())}
+          rightTitle="Add"
+          key="employeeList" 
+          component={EmployeeList} 
+          title="Employees" 
+        />
+        
+        <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
       </Scene>
     </Router>
   );
